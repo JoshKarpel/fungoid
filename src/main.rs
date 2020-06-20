647c8af34a7e67650f038442fa7031de15d341c2
Josh Karpel <josh.karpel@gmail.com>
2020-06-20 10:58:15 -0500

    better cli arg parsing (introduce subcommands)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 extern crate fungoid;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use std::io;
 use std::time::Duration;
 
@@ -10,36 +10,49 @@ fn main() {
         .version("0.1.0")
         .author("Josh Karpel <josh.karpel@gmail.com>")
         .about("A Befunge interpreter written in Rust")
-        .arg(
-            Arg::with_name("FILE")
-                .help("file to read program from")
-                .required(true)
-                .index(1),
+        .subcommand(
+            SubCommand::with_name("run")
+                .arg(Arg::with_name("time").long("time").help("enable timing"))
+                .arg(
+                    Arg::with_name("show")
+                        .long("show")
+                        .help("show program before executing"),
+                )
+                .arg(
+                    Arg::with_name("FILE")
+                        .help("file to read program from")
+                        .required(true),
+                ),
         )
-        .arg(Arg::with_name("time").long("time").help("enable timing"))
-        .arg(
-            Arg::with_name("show")
-                .long("show")
-                .help("show program before executing"),
-        )
-        .arg(
-            Arg::with_name("step")
-                .long("step")
-                .takes_value(true)
-                .default_value("10")
-                .help("step through program"),
+        .subcommand(
+            SubCommand::with_name("step")
+                .arg(
+                    Arg::with_name("rate")
+                        .long("rate")
+                        .takes_value(true)
+                        .default_value("10")
+                        .help("maximum instructions per second"),
+                )
+                .arg(
+                    Arg::with_name("FILE")
+                        .help("file to read program from")
+                        .required(true),
+                ),
         )
         .get_matches();
 
-    let filename = matches.value_of("FILE").unwrap();
+    if let Some(matches) = matches.subcommand_matches("step") {
+        let filename = matches.value_of("FILE").unwrap();
+        let program = fungoid::Program::from_file(&filename);
 
-    let program = fungoid::Program::from_file(&filename);
-
-    if matches.is_present("step") {
-        let max_ips: u32 = matches.value_of("step").unwrap().parse().unwrap();
+        let max_ips: u32 = matches.value_of("rate").unwrap().parse().unwrap();
         let dur = Duration::from_secs_f64(1.0 / (max_ips as f64));
+
         fungoid::step(program, dur).unwrap();
-    } else {
+    } else if let Some(matches) = matches.subcommand_matches("run") {
+        let filename = matches.value_of("FILE").unwrap();
+        let program = fungoid::Program::from_file(&filename);
+
         if matches.is_present("show") {
             program.show();
         }

8c3baaecc390433eebea6956da7c6eb2f47ddbc5
Josh Karpel <josh.karpel@gmail.com>
2023-10-13 20:54:45 -0500

    Upgrade deps (#66)

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,22 @@ use std::{
     io,
     io::{Read, Write},
     str::FromStr,
+    string::String,
     time::Instant,
 };
 
-use clap::{Arg, ArgMatches, Command};
+use clap::{
+    Arg,
+    ArgAction::{Set, SetTrue},
+    ArgMatches, Command,
+};
 use fungoid::{examples::EXAMPLES, execution::ExecutionState, program::Program};
 use humantime::format_duration;
 use itertools::Itertools;
 use separator::Separatable;
 
 fn main() {
-    if let Err(e) = _main() {
+    if let Err(e) = cli() {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
@@ -23,38 +28,48 @@ fn main() {
 
 type GenericResult = Result<(), Box<dyn std::error::Error>>;
 
-fn _main() -> GenericResult {
-    let matches = Command::new("fungoid")
+fn command() -> Command {
+    Command::new("fungoid")
         .version("0.2.1")
         .author("Josh Karpel <josh.karpel@gmail.com>")
         .about("A Befunge interpreter written in Rust")
         .subcommand(
             Command::new("run")
                 .about("Execute a program")
-                .arg(Arg::new("profile").long("profile").help("Enable profiling"))
+                .arg(
+                    Arg::new("profile")
+                        .long("profile")
+                        .action(SetTrue)
+                        .help("Enable profiling"),
+                )
                 .arg(
                     Arg::new("trace")
                         .long("trace")
+                        .action(SetTrue)
                         .help("Trace program execution"),
                 )
                 .arg(
                     Arg::new("FILE")
-                        .help("The file to read the program from")
-                        .required(true),
+                        .action(Set)
+                        .required(true)
+                        .help("The file to read the program from"),
                 ),
         )
         .subcommand(
             Command::new("ide").about("Start a TUI IDE").arg(
                 Arg::new("FILE")
-                    .help("The file to read the program from")
-                    .required(true),
+                    .action(Set)
+                    .required(true)
+                    .help("The file to read the program from"),
             ),
         )
         .subcommand(
             Command::new("examples").about("Print the names of the bundled example programs."),
         )
         .arg_required_else_help(true)
-        .get_matches();
+}
+fn cli() -> GenericResult {
+    let matches = command().get_matches();
 
     if let Some(matches) = matches.subcommand_matches("ide") {
         ide(matches)?;
@@ -91,7 +106,7 @@ impl Error for NoExampleFound {
 }
 
 fn load_program(matches: &ArgMatches) -> Result<Program, Box<dyn Error>> {
-    let file = matches.value_of("FILE").unwrap();
+    let file = matches.get_one::<String>("FILE").unwrap();
     if file.starts_with("example:") || file.starts_with("examples:") {
         let (_, e) = file.split_once(':').unwrap();
         if let Some(p) = EXAMPLES.get(e) {
@@ -121,14 +136,10 @@ fn run_program(matches: &ArgMatches) -> GenericResult {
 
     let input = &mut io::stdin();
     let output = &mut io::stdout();
-    let program_state = fungoid::execution::ExecutionState::new(
-        program,
-        matches.is_present("trace"),
-        input,
-        output,
-    );
+    let program_state =
+        fungoid::execution::ExecutionState::new(program, matches.get_flag("trace"), input, output);
 
-    run(program_state, matches.is_present("profile"))?;
+    run(program_state, matches.get_flag("profile"))?;
 
     Ok(())
 }
@@ -153,4 +164,14 @@ pub fn run<R: Read, O: Write>(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::command;
+
+    #[test]
+    fn verify_command() {
+        command().debug_assert();
+    }
 }
